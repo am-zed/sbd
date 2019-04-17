b499674e3cf1206d86e37eeafb233af3a444436c
am-zed <49406+am-zed@users.noreply.github.com>
2019-04-17 11:17:17 +0100

    Quick dirty update
    - use new base_url
    - hardcode cookies to be used
    - start with TOC page
    - update all links

--- sbd/sbd.py
+++ sbd/sbd.py
@@ -10,24 +10,10 @@ def remove_non_ascii_chars(text):
     return re.sub(r'[^\x00-\x7F]+', ' ', text)
 
 
-def get_credentials(arguments):
-    if arguments.login is not None and arguments.password is not None:
-        return arguments.login, arguments.password
-    else:
-        try:
-            return os.environ["SBD_LOGIN"], os.environ["SBD_PASSWORD"]
-        except KeyError:
-            print("You must provide Safari Books Online credentials")
-            exit()
-
-
-base_url = 'https://www.safaribooksonline.com/'
-login_url = 'https://www.safaribooksonline.com/accounts/login'
+base_url = 'https://learning.oreilly.com'
 
 parser = argparse.ArgumentParser(
     description='A small program to download books from Safari Books Online for offline storage.')
-parser.add_argument('-u', '--login', help='Safari Books Online login')
-parser.add_argument('-p', '--password', help='Safari Books Online password')
 parser.add_argument('safari_book_url',
                     help='Safari book url, ex. https://www.safaribooksonline.com/library/view/book-name/book-code/')
 
@@ -40,36 +26,48 @@ def main():
         print("URL is invalid, please pass proper URL")
         exit()
 
-    login, password = get_credentials(args)
+    cookies = [{ "BrowserCookie": "xxx" },
+            { "csrfsafari": "xxx" },
+            { "logged_in": "xxx" },
+            { "user_identifier": "xxx" },
+            { "sessionid": "xxx" }]
 
     br = RoboBrowser(parser='lxml')
-    br.open(login_url)
-    login_form = br.get_form()
-    login_form['email'].value = login
-    login_form['password1'].value = password
-    br.session.headers['Referer'] = login_url
-    br.submit_form(login_form)
+    br.open(url)
+
+    for cookie in cookies:
+        br.session.cookies.update(cookie)
 
     error_list = br.parsed.find_all("ul", class_='errorlist')
 
     if error_list.__len__() != 0:
-        print("Login has failed: " + error_list[0].contents[0].text)
+        print("Invalid cookies: " + error_list[0].contents[0].text)
         exit()
     else:
-        print("Login successful")
+        print("Valid cookies")
 
+    complete_book = ''
+
+    ## reopen URL with new cookies
     br.open(url)
+    ## include TOC page
+    content = br.parsed.find("section", {"class": "detail-book"})
+    for img in content.findAll('img'):
+        img['src'] = img['src'].replace("/library/", base_url + "/library/")
+    for links in content.findAll('a'):
+        links['href'] = links['href'].replace("/library/", base_url + "/library/")
+    complete_book += remove_non_ascii_chars(content.__str__())
+
     url_list = []
 
     for chapter in br.parsed.find_all("a", class_='t-chapter'):
-        url_list.append(base_url + chapter['href'])
+        url_list.append(chapter['href'])
 
     author = br.parsed.find('meta', {"property": 'og:book:author'})['content']
     title = br.parsed.find('meta', {"itemprop": 'name'})['content']
     author_title = author + ' - ' + title
     filename = str(author_title) + '.pdf'
 
-    complete_book = ''
     print('Downloading ' + author_title)
     # fetch all the book pages
     for x in range(0, url_list.__len__()):
@@ -77,7 +75,9 @@ def main():
         br.open(url_list[x])
         content = br.parsed.find("div", {"id": "sbo-rt-content"})
         for img in content.findAll('img'):
-            img['src'] = img['src'].replace("/library/", base_url + "library/")
+            img['src'] = img['src'].replace("/library/", base_url + "/library/")
+        for links in content.findAll('a'):
+            links['href'] = links['href'].replace("/library/", base_url + "/library/")
         complete_book += remove_non_ascii_chars(content.__str__())
 
     print("Generating pdf...")
